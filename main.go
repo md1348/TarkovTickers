793a50d4dd8562cc2c8180547fa900a06ade73b1
Matt <md1348@sent.com>
2024-02-19 20:26:05 -0800

    Parsing json into structs

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
-	"net/http"
+	"log"
 	"os"
-
-	"github.com/Khan/genqlient/graphql"
 )
+
+type Item struct {
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	ShortName string `json:"shortName"`
+}
+
+const tempJson = `{"5447a9cd4bdc2dbd208b4567": {"id": "5447a9cd4bdc2dbd208b4567","name": "Colt M4A1 5.56x45 assault rifle","shortName": "M4A1"}, "544fb45d4bdc2dee738b4568": {"id": "544fb45d4bdc2dee738b4568","name": "Salewa first aid kit","shortName": "Salewa"}}`
 
 func main() {
 	var err error
@@ -18,10 +24,23 @@ func main() {
 		}
 	}()
 
-	ctx := context.Background()
-	client := graphql.NewClient("https://api.tarkov.dev/graphql", http.DefaultClient)
-	resp, err := getItems(ctx, client, "GEN M3")
-	fmt.Println(resp.Items[0].Name, err)
-	fmt.Println(resp.Items[0].ShortName, err)
-	fmt.Println(resp.Items[0].Avg24hPrice, err)
+	data, err := os.ReadFile("items.en.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var items map[string]Item
+	err = json.Unmarshal(data, &items)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(items)
+
+	// ctx := context.Background()
+	// client := graphql.NewClient("https://api.tarkov.dev/graphql", http.DefaultClient)
+	// resp, err := getItems(ctx, client, "GEN M3")
+	// fmt.Println(resp.Items[0].Name, err)
+	// fmt.Println(resp.Items[0].ShortName, err)
+	// fmt.Println(resp.Items[0].Avg24hPrice, err)
 }
---
+++ todo.md
@@ -0,0 +1,12 @@
+# TarkovTickers
+
+## TODO
+- [ ] parse a single item from json into program
+- [ ] read json from tarkovdata on start
+    - copy into project dir
+- [ ] take search string from cl arg
+- [ ] fuzzy search arg against tarkovdata items
+- [ ] query tarkov.dev using ID
+- [ ] print avg24hPrice from response
+- [ ] find best measure of price available from API & query that 
+
